e325af93626c7525dadaf9376d49481df9533004
Wodecky <maciej.krebs@gmail.com>
2017-12-19 21:42:50 +0100

    initial commit

---
+++ Zespolona/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zespolona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zespolona<float> z1 = new Zespolona<float>(1.0f, 1.0f);
+            Zespolona<float> z2 = new Zespolona<float>(2.0f, 2.0f);
+
+            Zespolona<float> z3 = z1 + z2;
+
+            Zespolona<float> z4 = new Zespolona<float>(0, 0);
+
+            z4.przepisz(z3);
+           
+            Console.WriteLine(z3.Re);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zespolona/Zespolona.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zespolona
+{
+    public class Zespolona<T>:IComparable, ICloneable where T : IComparable
+    {
+        private T re;
+        private T im;
+
+        public T moduł;
+
+        public Zespolona(T re, T im)
+        {
+            this.re = re;
+            this.im = im;
+
+            this.moduł = this.Moduł();
+        }
+
+       private T Moduł()
+        {
+            return (T)Math.Sqrt((dynamic)this.re * (dynamic)this.re + (dynamic)this.im * (dynamic)this.im);
+        }
+
+        public T ModułBezDynamiców()
+        {
+            try
+            {
+                double re = Convert.ToDouble(this.re);
+                double im = Convert.ToDouble(this.im);
+
+                return (T)Convert.ChangeType(Math.Sqrt(re * re + im * im), typeof(T));
+            }
+            catch (Exception ex)
+            {
+                return (T)Convert.ChangeType(-1.0, typeof(T));
+            }
+        }
+
+       /* public Zespolona<T> Dodaj(Zespolona<T> z2)
+        {
+            return new Zespolona<T>(this.re + z2.re, this.im + z2.im);
+        }*/
+
+        public void przepisz(Zespolona<T> z)
+        {
+            this.im = z.Im;
+            this.re = z.Re;
+        }
+
+        public static Zespolona<T> operator +(Zespolona<T> lhs, Zespolona<T>rhs)
+        {
+            
+            return new Zespolona<T>((dynamic)lhs.re + (dynamic)rhs.re, (dynamic)lhs.im + (dynamic)lhs.im);
+        }
+
+        public T Re
+        {
+            set
+            {
+                this.re = value;
+            }
+
+            get
+            {
+                return this.re;
+            }
+        }
+
+        public T Im
+        {
+            set
+            {
+                this.im = value;
+            }
+
+            get
+            {
+                return this.im;
+            }
+        }
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
